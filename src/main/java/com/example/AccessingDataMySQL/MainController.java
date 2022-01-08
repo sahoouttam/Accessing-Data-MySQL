571ad091a4f8941d9d6b91008208d0ab89adc61d
UttamS <uttamsahoo6205@gmail.com>
2022-01-08 11:37:20 +0530

    my project

---
+++ src/main/java/com/example/AccessingDataMySQL/MainController.java
@@ -0,0 +1,33 @@
+package com.example.AccessingDataMySQL;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequestMapping(path = "/demo")
+public class MainController {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);        
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    
+}
---
+++ src/test/java/com/example/AccessingDataMySQL/AccessingDataMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessingDataMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
